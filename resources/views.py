ce02f1fae84b4400e0c8099472f5befa51bef480
N15H4N7 <ngupta_be19@thapar.edu>
2021-03-20 18:57:21 +0530

    Resource form

--- resources/views.py
+++ resources/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from .models import *
 from .forms import *
 
@@ -6,4 +7,10 @@ from .forms import *
 
 @login_required
 def all_subs(request):
-
+	resources = Resource.objects.filter(resource_by=request.user)
+	reminders = Reminder.objects.filter(reminder_by=request.user)
+	context = {
+		'resources' : resources,
+		'reminders'	: reminders,
+	}
+	return render(request, 'resources/resource_home.html', context)
